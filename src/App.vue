b3ac041d185a29dbea4c75f847f558258a2f9232
jungsungsoo <meseji87@gmail.com>
2022-01-19 23:17:01 +0900

    firebase 연동 및 vue 프로젝트 초기화

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <a href="https://jssvs.tistory.com/">데이터 엔지니어 기술 블로그</a>
+</template>
+
+<script>
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
